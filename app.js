aa547e34e32ebb6d17e85005369aaa7445e9f3c9
sharmivijay <sharmiyogu@gmail.com>
2021-09-21 22:18:33 +0530

    Updated with fetch api

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var userInput = document.querySelector("#input-txt");
+var inputText = document.querySelector("#input-txt");
 var outputText = document.querySelector(".output-txt");
 
-function callHandler(){
-    var translatedOutput;
-    fetch(translateUrl+"?text="+userInput.value)
+/**
+ * This funtion is an example of detailed function and I have written the same using arrow functions
+ * function callHandler(){
+    fetch(translateUrl+"?text="+inputText.value)
     .then(function (response)
     {
         if(response.ok){
@@ -17,10 +18,26 @@ function callHandler(){
     })
     .catch(errorHandler);
     
-};
+};**/
+
+function callHandler(){
+    var finalUrl= translateUrl+"?text="+inputText.value;
+    console.log(finalUrl);
+ fetch(finalUrl)
+ .then((response) => 
+    {
+        if(response.ok){
+            return response.json();
+        }
+        console.log(response);
+     throw new Error("Lets try after sometime");
+    })
+ .then(data => outputText.innerHTML = data.contents.translated )
+ .catch((error) => {alert("Error : " +error.message)});
+}
 
 function errorHandler(){
-    alert("Error occured while fetching the minion language");
+    alert("Please try after sometime");
 }
 
 btnTranslate.addEventListener("click",callHandler);
